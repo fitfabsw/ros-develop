8eed521e9a221b982d92d5bb791f93fbcf387ca5
zealzel <zealzel@gmail.com>
2024-09-15 23:16:44 +0800

    refactor: change colcon_build logic  - change colcon_build logic: git clone repos before extract_package  - add zbot_artic_humble_multi.repos & zbot_lino_humble_multi.repos  - use humble_multi repos in colcon_build_examples.sh

--- setup_vm/colcon_build_examples.sh
+++ setup_vm/colcon_build_examples.sh
@@ -15,24 +15,24 @@ if [[ $exit_code -ne 0 ]]; then
 	exit
 fi
 
-echo -e "\n=========install apriltag=============="
-source "$WORKSPACEPATH/install/setup.bash"
-REPOS_APRIL="$script_dir/apriltag/apriltag.repos"
-IGNORE_APRIL="nova_carter_docking,my_autodock"
-$colcon_build_sh -w $WORKSPACE -r "$REPOS_APRIL" -cdf ${TOKEN:+-t$TOKEN}
-cd "$WORKSPACEPATH/src/apriltag" || exit # install apriltag
-cmake -B build -DCMAKE_BUILD_TYPE=Release
-sudo cmake --build build --target install
-$colcon_build_sh -w $WORKSPACE -r "$REPOS_APRIL" -b --CLEAN_CACHE -i $IGNORE_APRIL ${TOKEN:+-t$TOKEN}
+# echo -e "\n=========install apriltag=============="
+# source "$WORKSPACEPATH/install/setup.bash"
+# REPOS_APRIL="$script_dir/apriltag/apriltag.repos"
+# IGNORE_APRIL="nova_carter_docking,my_autodock"
+# $colcon_build_sh -w $WORKSPACE -r "$REPOS_APRIL" -cdf ${TOKEN:+-t$TOKEN}
+# cd "$WORKSPACEPATH/src/apriltag" || exit # install apriltag
+# cmake -B build -DCMAKE_BUILD_TYPE=Release
+# sudo cmake --build build --target install
+# $colcon_build_sh -w $WORKSPACE -r "$REPOS_APRIL" -b --CLEAN_CACHE -i $IGNORE_APRIL ${TOKEN:+-t$TOKEN}
 
-echo -e "\n=========colcon build lino2=============="
-source "$WORKSPACEPATH/install/setup.bash"
-REPOS_LINO2="$script_dir/simulations_zbot_lino/zbot_lino_$ROSDISTRO.repos"
-IGNORE_LINO2="linorobot2_bringup"
-$colcon_build_sh -w $WORKSPACE -r "$REPOS_LINO2" -cdbf --CLEAN_CACHE -i $IGNORE_LINO2 ${TOKEN:+-t$TOKEN}
+# echo -e "\n=========colcon build lino2=============="
+# source "$WORKSPACEPATH/install/setup.bash"
+# REPOS_LINO2="$script_dir/simulations_zbot_lino/zbot_lino_"$ROSDISTRO"_multi.repos"
+# IGNORE_LINO2="linorobot2_bringup"
+# $colcon_build_sh -w $WORKSPACE -r "$REPOS_LINO2" -cdbf --CLEAN_CACHE -i $IGNORE_LINO2 ${TOKEN:+-t$TOKEN}
 
-echo -e "\n=========colcon build artic=============="
-source "$WORKSPACEPATH/install/setup.bash"
-REPOS_ARTIC="$script_dir/simulations_zbot_artic/zbot_artic_$ROSDISTRO.repos"
-IGNORE_ARTIC="gazebo_ros2_control_demos"
-$colcon_build_sh -w $WORKSPACE -r "$REPOS_ARTIC" -cdbf --CLEAN_CACHE -i $IGNORE_ARTIC ${TOKEN:+-t$TOKEN}
+# echo -e "\n=========colcon build artic=============="
+# source "$WORKSPACEPATH/install/setup.bash"
+# REPOS_ARTIC="$script_dir/simulations_zbot_artic/zbot_artic_"$ROSDISTRO"_multi.repos"
+# IGNORE_ARTIC="gazebo_ros2_control_demos"
+# $colcon_build_sh -w $WORKSPACE -r "$REPOS_ARTIC" -cdbf --CLEAN_CACHE -i $IGNORE_ARTIC ${TOKEN:+-t$TOKEN}
